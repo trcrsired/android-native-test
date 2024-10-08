88db8dd9b0f1ec3f200571d92506a3ec62d18fd8
trcrsired <uwgghhbcad@gmail.com>
2024-10-08 01:38:01 -0400

    g

--- genpack.sh
+++ genpack.sh
@@ -7,5 +7,8 @@ aapt package -f -M AndroidManifest.xml -I $ANDROID_SDK_ROOT/platforms/android-30
 #zipalign to read data directly not copy
 zipalign -f 4 apk-unsigned.apk apk-unsigned.apk
 
+#generate our keystore
+#keytool -genkey -v -keystore my-release-key.keystore -alias my-release-key -keyalg ed25519 -validity 100000
+
 #put our signature
-apksigner sign --out apk-signed.apk apk-unsigned.apk
+apksigner sign --ks ./my-release-key.keystore --ks-key-alias=my-release-key --out apk-signed.apk apk-unsigned.ap
