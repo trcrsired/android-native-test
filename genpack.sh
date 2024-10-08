53d12554d217bb3b361e7eca9161096a666bb7b6
trcrsired <uwgghhbcad@gmail.com>
2024-10-08 01:23:57 -0400

    pack

--- genpack.sh
+++ genpack.sh
@@ -1,1 +1,1 @@
-$ANDROID_SDK_ROOT/build-tools/34.0.0/aapt package -f -M AndroidManifest.xml -I $ANDROID_SDK_ROOT/platforms/android-30/android.jar
+$ANDROID_SDK_ROOT/build-tools/34.0.0/aapt package -f -M AndroidManifest.xml -I $ANDROID_SDK_ROOT/platforms/android-30/android.jar -S res -F apk-unaligned.apk apk
